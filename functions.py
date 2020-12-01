e173c40ad407786e5126ed5c1fe5d05dba68cdba
jimas95 <jimas95@hotmail.com>
2020-12-01 14:14:07 -0600

    added function of matrix --> list

--- functions.py
+++ functions.py
@@ -19,7 +19,10 @@ def display_init_conditions(init_conditions):
 def write_csv(data,name):
     """
     save data as csv file
-    csv form chassis phi, chassis x, chassis y, J1, J2, J3, J4, J5, W1, W2, W3, W4, gripper state
+    for robot state:
+        csv form chassis phi, chassis x, chassis y, J1, J2, J3, J4, J5, W1, W2, W3, W4, gripper state
+    for trajectory :
+        csv form r11,r12,r13,r21,r22,r23,r31,r32,r33,px,py,pz,gripper_state
     """
 
     # Open a file for output
@@ -41,6 +44,26 @@ def write_csv(data,name):
     # close file
     f.close()
 
+def convertToCsvForm(myList,data,gripper_state):
+    """
+    basicaly converts a 4x4 matrix to a list thats for csv format.
+    data contains a trajectory, N transformations of 4x4 matrix
+    appends myList with the values that we are intrested from the 4x4 matrix, in order to right them as csv
+    """
+    for row in data:
+        r11 = row[0][0]
+        r12 = row[0][1]
+        r13 = row[0][2]
+        r21 = row[1][0]
+        r22 = row[1][1]
+        r23 = row[1][2]
+        r31 = row[2][0]
+        r32 = row[2][1]
+        r33 = row[2][2]
+        px  = row[0][3]
+        py  = row[1][3]
+        pz  = row[2][3]
+        myList.append([r11,r12,r13,r21,r22,r23,r31,r32,r33,px,py,pz,gripper_state])
 
 def check_limits(input_state,joint_limit,wheel_limit):
     """
