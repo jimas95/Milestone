5b25f15cb2c11b80b4dbcc2ad686f88fc928380b
jimas95 <jimas95@hotmail.com>
2020-12-04 03:09:40 -0600

    added more matrixs

--- myMatrix.py
+++ myMatrix.py
@@ -11,26 +11,29 @@ class allMatrix:
         self.r = 0.0475
         wheel = self.w+self.l
         
-        self.H = np.array([[-1/(wheel), 1/(wheel), 1/(wheel), -1/(wheel)],[1,1,1,1],[-1,1,-1,1]])
+        self.H = np.array([ [-1/(wheel), 1/(wheel), 1/(wheel), -1/(wheel)],
+                            [1,1,1,1],
+                            [-1,1,-1,1]])
 
         self.F6 = np.zeros((6,4))
         self.F6[2:5] = self.r/4 * self.H
 
-        #transformation from ....
-        self.B_list_arm = np.array([ [0,0,0,0,0],
+#       When the arm is at its home configuration, the screw axes
+        self.Blist = np.array([ [0,0,0,0,0],
                                 [0,-1,-1,-1,0],
                                 [1,0,0,0,1],
                                 [0,-0.5076,-0.3526,-0.2176,0],
                                 [0.033,0,0,0,0],
                                 [0,0,0,0,0]])
-        #transformation from base of robot to robotic arm
+#       The fixed offset from the chassis frame {b} to the base frame of the arm {0} 
         self.Tb0 = np.array([[1,0,0,0.1662],
-                                    [0,1,0,0],
-                                    [0,0,1,0.0026],
-                                    [0,0,0,1]])
+                            [0,1,0,0],
+                            [0,0,1,0.0026],
+                            [0,0,0,1]])
 
-        #transformation from base of the robotic arm to eef 
-        self.M0e = np.array([ [1,0,0,0.033],
-                                    [0,1,0,0],
-                                    [0,0,1,0.6546],
-                                    [0,0,0,1]])
+#       When the arm is at its home configuration (all joint angles zero) 
+#       the end-effector frame {e} relative to the arm base frame {0}
+        self.M0e =np.array([[1,0,0,0.033],
+                            [0,1,0,0],
+                            [0,0,1,0.6546],
+                            [0,0,0,1]])
